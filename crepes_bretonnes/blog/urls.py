b9f427ad4a2664f23dbae4f7b4510420708a0c15
tchab0 <thierry.chabot@gmail.com>
2019-03-26 10:56:01 +0100

    Mon Appli Django

---
+++ crepes_bretonnes/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('date', views.date_actuelle),
+    path('addition/<int:nombre1>/<int:nombre2>/', views.addition)
+]
---
+++ crepes_bretonnes/blog/views.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from django.shortcuts import render
+
+def date_actuelle(request):
+    return render(request, 'blog/date.html', {'date': datetime.now()})
+
+
+def addition(request, nombre1, nombre2):    
+    total = nombre1 * nombre2
+
+    # Retourne nombre1, nombre2 et la somme des deux au tpl
+    return render(request, 'blog/addition.html', locals())
+    
+couleurs = ['rouge', 'orange', 'jaune', 'vert', 'bleu', 'indigo', 'violet']
